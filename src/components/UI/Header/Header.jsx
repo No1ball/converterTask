3e5e906c4c15bc6050dd3296b7a572ce5d9bd1b8
lesha <lastchangenik@gmail.com>
2023-03-09 00:54:49 +0300

    add new component

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import classes from "./Header.module.scss";
 import Navigation from "../Navigation/Navigation";
 const Header = () => {
-    const data = [{text:'Курсы валют', href:'info'}, {text:'Конвертор валют', href:'converter'}, {text:'Начальная страница', href:'home'}]
+    const data = [{text:'Курсы валют', href:'info'},
+                 {text:'Конвертор валют', href:'converter'},
+                 {text:'Начальная страница', href:'home'}]
     return (
         <div className={classes.headerCl}>
             <div className={classes.logoAndName}>
