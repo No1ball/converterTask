5c5b8a12999d037293fed85e5b72280443270aa8
No1ball <noonepoint@gmail.com>
2023-03-18 16:01:38 +0300

    fixed error

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -3,13 +3,13 @@ import classes from "./Header.module.scss";
 import Navigation from "../Navigation/Navigation";
 const Header = () => {
     const data = [{text:'Курсы валют', href:'info'},
-                 {text:'Конвертор валют', href:'converter'}]
+                 {text:'Конвертер валют', href:'converter'}]
 
     return (
         <div className={classes.headerCl}>
             <div className={classes.logoAndName}>
                 <img alt='Лого' src={require('../../../603655-200.png')}/>
-                TaskConvertor
+                TaskConverter
             </div>
             <Navigation data={data}/>
         </div>
